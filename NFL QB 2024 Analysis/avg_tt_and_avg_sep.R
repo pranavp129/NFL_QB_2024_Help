1b1b1e9e071260037ac28c3cbd9ccb57eeb5191a
Pranav Pitchala <pitchala.pranav@gmail.com>
2025-04-03 17:41:18 -0700

    The size of each point is now based on the passing epa for each QB
    In addition to the EPA, I fixed a bug where Stafford was missing a bubble since the team colors were not loaded due a different in team abbreviation (LA vs LAR)

--- NFL QB 2024 Analysis/avg_tt_and_avg_sep.R
+++ NFL QB 2024 Analysis/avg_tt_and_avg_sep.R
@@ -12,12 +12,13 @@ colnames(nextgen_passing)
 
 nextgen_passing_season <- nextgen_passing %>% 
   filter(week != 0) %>% 
-  select(week, player_display_name, team_abbr, avg_time_to_throw)
+  select(week, player_gsis_id, player_display_name, team_abbr, avg_time_to_throw)
 
 # get receiving stats
 nextgen_receiving <- nflreadr::load_nextgen_stats(seasons = 2024, stat_type = "receiving")
 colnames(nextgen_receiving)
 
+# group receiving data by team and week
 nextgen_receiving_season <- nextgen_receiving %>% 
   filter(week != 0) %>% 
   group_by(team_abbr, week) %>% 
@@ -32,11 +33,13 @@ season_ngs_data <- combined_ngs_data %>%
   group_by(player_display_name, team_abbr) %>% 
   summarise(season_avg_time_to_throw = mean(avg_time_to_throw, na.rm = TRUE),
             season_avg_team_separation = mean(avg_team_separation, na.rm = TRUE),
-            weeks_played = n())
+            weeks_played = n(),
+            player_gsis_id = first(player_gsis_id))
 
 # get team colors and combine
 teams <- nflreadr::load_teams(current = TRUE) %>%
-  select(team_abbr, team_color, team_color2)
+  select(team_abbr, team_color, team_color2) %>%
+  mutate(team_abbr = recode(team_abbr, "LA" = "LAR"))  # Adjust LA to LAR for Rams
 
 season_ngs_data <- season_ngs_data %>% 
   left_join(teams, "team_abbr")
@@ -45,10 +48,10 @@ season_ngs_data <- season_ngs_data %>%
 season_ngs_data <- season_ngs_data %>% 
   filter(weeks_played >= 3)
 
-# plot
+# plot (without epa)
 ggplot(data = season_ngs_data, aes(x = season_avg_time_to_throw, y = season_avg_team_separation)) +
   geom_mean_lines(aes(x0 = season_avg_time_to_throw, y0 = season_avg_team_separation), 
-                  size = 0.8,
+                  linewidth = 0.8,
                   color = "black", 
                   linetype = "dashed",
                   alpha = 0.5) +
@@ -59,7 +62,6 @@ ggplot(data = season_ngs_data, aes(x = season_avg_time_to_throw, y = season_avg_
   geom_text_repel(aes(label = player_display_name),
                   box.padding = 0.6,
                   size = 3,
-                  family = "Roboto",
                   fontface = "bold") +
   scale_x_continuous(breaks = scales::pretty_breaks(n = 6),
                      labels = scales::label_comma()) +
@@ -69,5 +71,55 @@ ggplot(data = season_ngs_data, aes(x = season_avg_time_to_throw, y = season_avg_
        y = "Average Separation of Receivers (yards)",
        title = "How Easy a Quarterback's Job was in 2024",
        subtitle = "Average Time to Throw vs. Average Separation of Receivers",
+       caption = "Data: Next Gen Stats, NFLReadR\n*Min. 3 games played\nPranav Pitchala") +
+  nfl_analytics_theme()
+
+# load player stats for QB EPA
+player_stats <- nflreadr::load_player_stats(seasons = 2024)
+colnames(player_stats)
+
+# get passing epa, normalize passing epa between 1-20
+min_size = 1
+max_size = 20
+qb_epa_stats <- player_stats %>% 
+  filter(position == "QB") %>% 
+  group_by(player_display_name) %>% 
+  summarise(player_gsis_id = first(player_id),
+            total_pass_epa = sum(passing_epa, na.rm = TRUE)) %>% 
+  mutate(normalized_epa = (total_pass_epa - min(total_pass_epa)) / 
+           (max(total_pass_epa) - min(total_pass_epa)),
+         # Adjusting the size range by adding a constant (e.g., 1) to ensure the minimum size is not zero
+         normalized_epa_size = normalized_epa * (max_size - min_size) + min_size)
+
+# sort data by normalized_epa_size so that larger points are printed first
+season_ngs_data_with_epa <- season_ngs_data %>% 
+  left_join(qb_epa_stats, "player_gsis_id") %>% 
+  arrange(-normalized_epa_size)  # Sorting by EPA size
+
+# plot data with passing epa as point size
+ggplot(data = season_ngs_data_with_epa, aes(x = season_avg_time_to_throw, y = season_avg_team_separation)) +
+  geom_mean_lines(aes(x0 = season_avg_time_to_throw, y0 = season_avg_team_separation), 
+                  linewidth = 0.8,
+                  color = "black", 
+                  linetype = "dashed",
+                  alpha = 0.5) +
+  geom_point(shape = 21,
+             fill = season_ngs_data_with_epa$team_color,
+             color = season_ngs_data_with_epa$team_color2,
+             size = season_ngs_data_with_epa$normalized_epa_size) +
+  geom_text_repel(aes(label = player_display_name.x),
+                  box.padding = 1.1,
+                  arrow.padding = .5,
+                  force = 10,
+                  size = 4,
+                  fontface = "bold") +
+  scale_x_continuous(breaks = scales::pretty_breaks(n = 6),
+                     labels = scales::label_comma()) +
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 6),
+                     labels = scales::label_comma()) +
+  labs(x = "Average Time to Throw (seconds)",
+       y = "Average Separation of Receivers (yards)",
+       title = "Quarterback Performance: Time to Throw vs. Receiver Separation",
+       subtitle = "Passing EPA Represented by Circle Size",
        caption = "Data: Next Gen Stats, NFLReadR\n*Min. 3 games played\nPranav Pitchala") +
   nfl_analytics_theme()
